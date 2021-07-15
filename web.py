60cc39a4e9d05d20912cc4c397f488918b444e4c
itisdb <dhriteshbhagat@gmail.com>
2021-07-15 22:56:46 +0530

    update web.py

--- web.py
+++ web.py
@@ -1,31 +1,36 @@
-from flask import Flask,render_template,Response
+#Import necessary libraries
+from flask import Flask, render_template, Response
 import cv2
+#Initialize the Flask app
+app = Flask(__name__)
 
-app=Flask(__name__)
+camera = cv2.VideoCapture('m3u8\\video.m3u8')
+'''
+for ip camera use - rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' 
+for local webcam use cv2.VideoCapture(0)
+'''
 
-camera=cv2.VideoCapture('m3u8\\video.m3u8')
-
-def generate_frames():
+def gen_frames():  
     while True:
-            
-        ## read the camera frame
-        success,frame=camera.read()
+        success, frame = camera.read()  # read the camera frame
         if not success:
             break
         else:
-            buffer=cv2.imencode('.jpg',frame)
-            frame=buffer.tobytes()
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
-        yield(b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/video')
-def video():
-    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
