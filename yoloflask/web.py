f264735e8168f0711536f959ca2ec27842d6f9df
itisdb <dhriteshbhagat@gmail.com>
2021-07-17 01:56:29 +0530

    rtsp url space

--- yoloflask/web.py
+++ yoloflask/web.py
@@ -6,11 +6,11 @@ import YOLOV5_Detection as yolo
 #Initialize the Flask app
 app = Flask(__name__)
 
-camera = cv2.VideoCapture('Sample1.mp4')
+#camera = cv2.VideoCapture('Sample1.mp4')
 '''
 for ip camera use - rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' 
 for local webcam use cv2.VideoCapture(0)
-'''
+
 
 def gen_frames():  
     while True:
@@ -24,6 +24,8 @@ def gen_frames():
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+'''
+
 
 @app.route('/')
 def index():
@@ -31,7 +33,7 @@ def index():
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(yolo.run(source='rtsp://192.168.29.88:8080/h264.sdp',view_img=True), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(yolo.run(source='your rtsp url',view_img=True), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == "__main__":
