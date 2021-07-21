42037ac135b69cb845ac0301a57d97a1ffc3f43f
itisdb <dhriteshbhagat@gmail.com>
2021-07-22 01:34:03 +0530

    update for v5

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# **YOLO.V3 and YOLO.v5**
+# **YOLO.v5 : Object Detection (Fire-Smoke Major)**
 
 # **In real time using Flask**
 
@@ -12,18 +12,10 @@
 
 - ![image](https://user-images.githubusercontent.com/53273486/126006975-c76f5607-2da4-430b-b902-6953eedd54fb.png)
 
-**This is for v3. Change the value of source to the rtsp/rtmp/https url (make sure they are in single quotes)**
+**This is for v5. Change the value of src to the rtsp/rtmp/https url (make sure they are in single quotes)**
 
-- ![image](https://user-images.githubusercontent.com/53273486/126007015-e7b20c95-4178-4de0-8133-ced2cff93d95.png)
+**Run the Script**
 
-**This is for v5. Enter you rtsp/rtmp/https url in place of <your rtsp url> in the argument section of the function yolo.run(), make sure they are in single quotes**
-
-**This repository has a both the YOLO.V3 and the YOLO.V5 in it.**
-
-- **YOLO.V3: it is present in the actual folder. To execute it, run this in the terminal/powershell:**
-  - **venv\Scripts\activate**
-  - **python web.py**
-- **YOLO.V5: it is present in the yolloFlask subfolder. To execute yolo v5, run this in the terminal/powershell:**
-  - **cd yoloflask**
-  - **venv\Scripts\activate**
-  - **python web.py**
+- **python web.py**
+- **Click on the local host link in the Terminal**
+- **Enjoy the Stream : Now u can differ among objects**
--- web.py
+++ web.py
@@ -2,30 +2,34 @@
 from flask import Flask, render_template, Response
 import cv2
 import os
-from Yolo_Demo_WCF import YOLO_OBJECT_DETECTION
-
+import detect as yolo
 #Initialize the Flask app
 app = Flask(__name__)
-source=0   #change this source when changing every stuff.
-camera = cv2.VideoCapture(source)
+
 '''
+camera = cv2.VideoCapture('Sample1.mp4')
 for ip camera use - rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' 
 for local webcam use cv2.VideoCapture(0)
 
-#this is for the generator function of the app when just live stream
+
 def gen_frames():  
     while True:
         success, frame = camera.read()  # read the camera frame
         #ret, buffer=yolo.run(source=frame)
-        #buffer=YOLO_OBJECT_DETECTION(True,frame)
+        ret, buffer = cv2.imencode('.jpg', frame)
+        
         if not success:
             break
         else:
-            ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 '''
+
+
+#Change this to your source file _ 0: webcam, or paste the rtsp url here
+#src='your rtsp url here'
+src='https://192.168.29.227:8080/video'
 
 @app.route('/')
 def index():
@@ -33,7 +37,7 @@ def index():
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(YOLO_OBJECT_DETECTION(True,source), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(yolo.run(source=src,view_img=True), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == "__main__":
